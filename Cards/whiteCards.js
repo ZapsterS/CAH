4c154b17fb558c0c7697691d9f417930067701b3
Zapster <findingslimes@gmail.com>
2021-08-15 22:40:50 -0400

    Create whiteCards.js

---
+++ Cards/whiteCards.js
@@ -0,0 +1,14 @@
+//White cards (Answers)
+
+var cards_whiteCards = [
+   "Test Answer One?",
+   "Test Answer Two?",
+   "Test Answer Three?",
+   "Test Answer Four?",
+   "Test Answer Five?",
+   "Test Answer Six?",
+   "Test Answer Seven?",
+   "Test Answer Eight?",
+   "Test Answer Nine?",
+   "Test Answer Ten?"
+ ]
